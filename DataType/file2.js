34d0da6786e35ff2870b71c4b9f4495685d70107
Sumaya Afrin Era <afrinera432@gmail.com>
2025-10-03 22:09:05 +0600

    Add files via upload

---
+++ DataType/file2.js
@@ -0,0 +1,12 @@
+"use strict";
+//data Type
+Object.defineProperty(exports, "__esModule", { value: true });
+//Array :
+let score = [20, 10, 50];
+console.log(score);
+let name = ["Sumaya", "Afrin"];
+console.log(name);
+//tuples :
+let student = ["Sumaya", 20, true];
+console.log(student);
+//# sourceMappingURL=file2.js.map
---
+++ DataType/file2.ts
@@ -0,0 +1,13 @@
+//data Type
+
+//Array :
+let score: number[]=[20,10,50]
+console.log(score);
+
+let name: string[]=["Sumaya","Afrin"]
+console.log(name);
+
+//tuples :
+
+let student: [string,number,boolean]=["Sumaya",20,true]
+console.log(student);
